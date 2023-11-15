6591cb9334c3357f56f6241d2f371f27ab28af77
Dmitrii Gadeev <kruftik@users.noreply.github.com>
2023-11-15 14:37:28 +0100

    use auth_user instead of user option in the default db config (#56)
    * use auth_user instead of user option in the default db config
    
    * multiuser, multidb usage example

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,7 +67,7 @@ if [ ! -f ${PG_CONFIG_DIR}/pgbouncer.ini ]; then
 ################## Auto generated ##################
 [databases]
 ${DB_NAME:-*} = host=${DB_HOST:?"Setup pgbouncer config error! You must set DB_HOST env"} \
-port=${DB_PORT:-5432} user=${DB_USER:-postgres}
+port=${DB_PORT:-5432} auth_user=${DB_USER:-postgres}
 ${CLIENT_ENCODING:+client_encoding = ${CLIENT_ENCODING}\n}\
 
 [pgbouncer]

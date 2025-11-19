c7323223e411babdf0b5cf2a9cdaa77702b4ecd3
Aniket Kumar <44335648+crypticani@users.noreply.github.com>
2025-11-19 19:51:55 +0000

    feat: added support for logfile using env (#118)

--- entrypoint.sh
+++ entrypoint.sh
@@ -146,6 +146,7 @@ ${STATS_PERIOD:+stats_period = ${STATS_PERIOD}\n}\
 ${VERBOSE:+verbose = ${VERBOSE}\n}\
 admin_users = ${ADMIN_USERS:-postgres}
 ${STATS_USERS:+stats_users = ${STATS_USERS}\n}\
+${LOGFILE:+logfile = ${LOGFILE}\n}\n
 
 # Connection sanity checks, timeouts
 ${SERVER_RESET_QUERY:+server_reset_query = ${SERVER_RESET_QUERY}\n}\
